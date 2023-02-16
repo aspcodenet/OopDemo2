64355476c20e0e78b3b576caf0dbc50c241e2e10
aspcodenet <stefan.holmberg@systementor.se>
2023-02-16 08:44:06 +0100

    Add project files.

---
+++ OopDemo/App.cs
@@ -0,0 +1,31 @@
+﻿using OopDemo;
+
+public class App
+{
+    public void Run()
+    {
+
+  
+
+        var yellowHouse = new House();
+        yellowHouse.Address = "Testgatan 12";
+        yellowHouse.Color = "Yellow";
+
+        var stefansHouse = new House();
+        stefansHouse.Address = "Testgatan 13";
+        stefansHouse.Color = "Brown";
+
+        var annasHouse = new House();
+        annasHouse.Address = "Testgatan 14";
+        annasHouse.Color = "Green";
+
+
+
+
+
+        stefansHouse.Color = "red";
+
+
+
+    }
+}
---
+++ OopDemo/House.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopDemo
+{
+    // Detta är en RITNING 
+    public class House
+    {
+        public string Address { get; set; }
+
+        public string Color { get; set; }
+
+
+    }
+}
